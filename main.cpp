dbbd235c4cd32e65ba72a9e4665705e8374babaf
Edward Choi <edwardchoi2001@gmail.com>
2025-10-05 21:11:14 -0700

    final update

--- main.cpp
+++ main.cpp
@@ -93,122 +93,42 @@ int main() {
     vector<string> comments;
     string line;
 
-    //pointer settings
-    ReviewNode* head = nullptr;
-    ReviewNode* tail = nullptr;
-
-    int choice = 0;
-    cout << "Which linked list method should we use?" << endl;
-    cout << "[1] New nodes are added at the head of the linked list" << endl;
-    cout << "[2] New nodes are added at the tail of the linked list" << endl;
-    cout << "Choice: ";
-    cin >> choice;
-    // input verification
-    while (cin.fail() || (choice != 1 && choice != 2)){
-        cout << "Invalid input. Please enter 1 or 2: ";
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cin >> choice;    
+    if (!commentFile) {
+        cerr << "Error: Could not open comments.txt. Please ensure the file exists in the same directory." << endl;
+        return 1;
     }
-    char another = 'y';
-    do {
-        double rating = -1.0;
-        string comment;
 
-        // get the review rating from the user with validation
-        cout << "Enter review rating 0-5: ";
-        cin >> rating;
-        while (cin.fail() || rating < 0 || rating > 5) {
-             cout << "Invalid rating. Please enter a number between 0 and 5: ";
-             cin.clear();
-             cin.ignore(numeric_limits<streamsize>::max(), '\n');
-             cin >> rating;
+    while (getline(commentFile, line)) {
+        if (!line.empty()) {
+            comments.push_back(line);
         }
+    }
+    commentFile.close();
 
-        // get the review comment
-        cout << "Enter review comments: ";
-        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clear the newline character left by cin
-        getline(cin, comment);
+   
 
-        // add the new node based on the user's initial choice
-        if (choice == 1) {
-            addNodeToHead(head, tail, rating, comment);
-        } else { 
-            addNodeToTail(head, tail, rating, comment);
+    // create a container of Movie objects
+    vector<Movie> movies;
+    movies.emplace_back("Inception");
+    movies.emplace_back("The Matrix");
+    movies.emplace_back("Parasite");
+    movies.emplace_back("The Grand Budapest Hotel");
+
+    // add at least three reviews to each movie object
+    int commentIndex = 0;
+    for (auto& movie : movies) {
+        for (int i = 0; i < 3; ++i) {
+            if(commentIndex < comments.size()){
+                movie.addReview(comments[commentIndex]);
+                commentIndex++;
+            }
         }
+    }
 
-        // ask if the user wants to enter another review
-        cout << "Enter another review? Y/N: ";
-        cin >> another;
-
-    } while (another == 'y' || another == 'Y');
-
-    
-    printListAndAverage(head);
-
-    
-    cleanup(head);
+    // print all the reviews for each movie
+    for (const auto& movie : movies) {
+        movie.printReviews();
+    }
 
     return 0;
-}
-
-
-void addNodeToTail(ReviewNode*& head, ReviewNode*& tail, double rating, const string& comment) {
-    ReviewNode* newNode = new ReviewNode{rating, comment, nullptr};
-    if (head == nullptr) {
-        // if the list is empty, the new node is both the head and the tail
-        head = newNode;
-        tail = newNode;
-    } else {
-        // else link the current tail to the new node and update tail
-        tail->next = newNode;
-        tail = newNode;
-    }
-}
-
-void addNodeToHead(ReviewNode*& head, ReviewNode*& tail, double rating, const string& comment) {
-    ReviewNode* newNode = new ReviewNode{rating, comment, nullptr};
-    if (head == nullptr) {
-        // If the list is empty, the new node is both the head and the tail
-        head = newNode;
-        tail = newNode;
-    } else {
-        // Otherwise, link the new node to the current head and update head
-        newNode->next = head;
-        head = newNode;
-    }
-}
-
-void printListAndAverage(ReviewNode* head) {
-    if (head == nullptr) {
-        cout << "\nNo reviews to display." << endl;
-        return;
-    }
-
-    cout << "\nOutputting all reviews:" << endl;
-    ReviewNode* current = head;
-    double totalRating = 0.0;
-    int count = 0;
-
-    while (current != nullptr) {
-        count++;
-        cout << "> Review #" << count << ": " << current->rating << ": " << current->comment << endl;
-        totalRating += current->rating;
-        current = current->next;
-    }
-
-    if (count > 0) {
-        double average = totalRating / count;
-        cout << "> Average: " << fixed << setprecision(6) << average << endl;
-    }
-}
-
-void cleanup(ReviewNode*& head) {
-    ReviewNode* current = head;
-    while (current != nullptr) {
-        ReviewNode* nextNode = current->next; 
-        delete current;                       
-        current = nextNode;                   
-    head = nullptr; 
-    }
 }
